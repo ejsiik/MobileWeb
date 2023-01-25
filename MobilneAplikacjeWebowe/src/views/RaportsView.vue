3fad2b266ff3707fd6c43c301e4a5ae400823163
ReinaArionAlkira <user@user.pl>
2023-01-25 01:39:01 +0100

    display reports style changed

--- MobilneAplikacjeWebowe/src/views/RaportsView.vue
+++ MobilneAplikacjeWebowe/src/views/RaportsView.vue
@@ -25,23 +25,29 @@ connection.getDoneTasksFromCurrentUserHierarchy()
 </template>
 
 <style>
-
 ul#category {
   list-style: none;
   padding: 0;
 }
-
-ul#list {
-  list-style: none;
-  padding: 0;
-}
-
-ul#list li {
+ul#category li{
+  text-align: center;
   padding: 0.5rem;
   margin: 0.5rem;
   background-color: #F1F1F1;
   border-radius: 0.2rem;
   width: 70%;
   color: #2A3B6F;
+  font-weight: bold;
+}
+ul#list li {
+  align-items: center;
+  justify-content: center;
+  text-align: left;
+  padding: 0.5rem;
+  margin: 0.5rem;
+  background-color: #2A3B6F;
+  border-radius: 0.2rem;
+  width:calc(100% - 1rem);
+  color: #F1F1F1;
 }
 </style>
