0162c13a199b4a4ff4a5f5883098ed365e4cf81b
Dawid Wydra <72543889+ejsiik@users.noreply.github.com>
2023-01-26 14:41:22 +0100

    AddTaskPanel view changed

--- MobilneAplikacjeWebowe/src/components/AddTaskPanel.vue
+++ MobilneAplikacjeWebowe/src/components/AddTaskPanel.vue
@@ -1,47 +1,41 @@
 <script setup>
-var counter = 4;
-
-function addElement() {
-  var completelist = document.getElementById("thelist");
-
-  completelist.innerHTML += "<li>Item " + counter + "</li>";
-  counter++;
-}
+const props = defineProps(['data'])
 
 </script>
 
+
 <template>
-  <div class="addTask">
-    <button onclick="addElement">Add Item</button>
-    <ul id="thelist">
-      <li>Item 1</li>
-      <li>Item 2</li>
-      <li>Item 3</li>
-    </ul>
-  </div>
+  <ul id="list_add_task">
+      <li v-if="data" v-for="item in Object.keys(data)">
+          {{ item }}
+      </li>
+      <li v-else>
+          Ładowanie...
+      </li>
+  </ul>
 </template>
 
 <style>
-ul#thelist {
-  list-style: none;
-  padding: 0;
+ul#list_add_task {
+list-style: none;
+padding: 0;
 }
 
-ul#thelist li {
-  padding: 0.5rem;
-  margin: 0.5rem;
-  background-color: #F1F1F1;
-  border-radius: 0.2rem;
-  width: 70%;
-  color: #2A3B6F;
+ul#list_add_task li {
+padding: 0.5rem;
+margin: 0.5rem;
+background-color: #F1F1F1;
+border-radius: 0.2rem;
+width: 70%;
+color: #2A3B6F;
 }
 
 button {
-  margin: 15% 30%;
-  background-color: #FFFFFF;
-  border-radius: 0.2rem;
-  text-align: center;
-  display: block;
-  color: #2A3B6F;
+margin: 15% 30%;
+background-color: #FFFFFF;
+border-radius: 0.2rem;
+text-align: center;
+display: block;
+color: #2A3B6F;
 }
 </style>
--- MobilneAplikacjeWebowe/src/views/AddTaskView.vue
+++ MobilneAplikacjeWebowe/src/views/AddTaskView.vue
@@ -1,6 +1,68 @@
 <script setup>
 import AddTaskPanel from '../components/AddTaskPanel.vue';
+import { reactive } from 'vue';
+import { connection } from '../backend-connection/connection.js';
+
+const doneTasksState = reactive({ data: null });
+
+connection.getDoneTasksFromCurrentUserHierarchy()
+.then(data => {
+    doneTasksState.data = data;
+    console.log(data);
+})
+.catch(e => {
+    alert("Błąd wczytywania danych");
+});
+
 </script>
+
 <template>
-    <AddTaskPanel/>
+    <div id = "wrapper">
+    <ul id = "category">
+        <li>Computer<AddTaskPanel :data="doneTasksState.data?.tasks?.Computer" /></li>
+        <li>Console<AddTaskPanel :data="doneTasksState.data?.tasks?.Console" /></li>
+        <li>Phone<AddTaskPanel :data="doneTasksState.data?.tasks?.Phone" /></li>
+    </ul>
+</div>
 </template>
+
+<style>
+#wrapper {
+  width:100%;
+  display:flex;
+  align-items: center;
+  justify-content: center;
+}
+ul#category {
+  flex:1;
+  list-style: none;
+  padding: 0;
+}
+ul#category li{
+  flex:1;
+  text-align: center;
+  padding: 0.5rem;
+  margin: 0.5rem;
+  background-color: #F1F1F1;
+  border-radius: 0.2rem;
+  color: #2A3B6F;
+  font-weight: bold;
+}
+ul#list_add_task li {
+  align-items: center;
+  justify-content: center;
+  text-align: left;
+  padding: 0.5rem;
+  margin: 0.5rem;
+  background-color: #2A3B6F;
+  border-radius: 0.2rem;
+  width:calc(100% - 1rem);
+  color: #F1F1F1;
+}
+@media only screen and (orientation: landscape) {
+    ul#list_add_task li {
+        text-align: center;
+    }
+}
+
+</style>
